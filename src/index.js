e2d38e4ec147faadb83489d5ae827a6a1a245d15
linhlt311 <ltlinh311@gmail.com>
2019-04-16 17:38:05 +0700

    during config

--- src/index.js
+++ src/index.js
@@ -1,6 +1,28 @@
-// @flow
-const { addPollListener } = require('./listener');
+// // @flow
+// const { addPollListener } = require('./listener');
 
-module.exports = (robot /* : Robot */) => {
-  robot.on(['issues.opened', 'issues.edited'], addPollListener);
-};
+// module.exports = (robot /* : Robot */) => {
+//   robot.on(['issues.opened', 'issues.edited'], addPollListener);
+// };
+
+/**
+ * This is the main entrypoint to your Probot app
+ * @param {import('probot').Application} app
+ */
+module.exports = app => {
+  // Your code here
+  app.log('Yay, the app was loaded!')
+  app.log("here");
+
+  app.on('issues.edited', async context => {
+    app.log("here2");
+    const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
+    return context.github.issues.createComment(issueComment)
+  })
+
+  // For more information on building apps:
+  // https://probot.github.io/docs/
+
+  // To get your app running against GitHub, see:
+  // https://probot.github.io/docs/development/
+}
